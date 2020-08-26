b98ee6ecb5341ea142e4930b7e8407113490183b
daign <jakob.mischek@daign.de>
2020-08-26 22:51:57 +0200

    added export for classes from subfolders

--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,6 @@ export { GraphicNode } from './graphicNode';
 export { NamedCollection } from './namedCollection';
 export { PresentationNode } from './presentationNode';
 export { View } from './view';
+
+export { MatrixTransform } from './transformations/matrixTransform';
+export { TransformCollection } from './transformations/transformCollection';
